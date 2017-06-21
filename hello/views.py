1236a3e87869ff1189e54e8dc5d4d808cf7b67f7
Zach Helfinstein <zhelfinstein@umail.ucsb.edu>
2017-06-21 15:23:46 -0700

    modified hello world message

--- hello/views.py
+++ hello/views.py
@@ -8,12 +8,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES', 3))
-    return HttpResponse('Hello! ' * times)
-    #r = requests.get('http://httpbin.org/status/418')
-    #print(r.text)
-    #return HttpResponse('<pre>' + r.text + '</pre>')
-    #return render(request, 'index.html')
+    return HttpResponse('Hello World! ')
 
 
 def db(request):
