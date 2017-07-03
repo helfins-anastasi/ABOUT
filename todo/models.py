1c48ea9cc9acf8b95da284bff9a2b972fece8fa1
Zach Helfinstein <zhelfinstein@umail.ucsb.edu>
2017-07-03 15:36:19 -0700

    tweaked database setup

--- todo/models.py
+++ todo/models.py
@@ -8,18 +8,31 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Item(models.Model):
     user = models.ForeignKey(User)
     priority = models.PositiveSmallIntegerField()
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return '%s (%s #%d)' % (self.description, self.user, self.priority)
+
 
 class Restriction(models.Model):
     MIN_DUR = "min"
     MAX_DUR = "max"
-    CHOICES = ((MIN_DUR, "Minimum Duration"), (MAX_DUR, "Maximum Duration"))
+    CHOICES = ((MIN_DUR, "Minimum Duration (minutes)"), (MAX_DUR, "Maximum Duration (minutes)"))
 
     item = models.ForeignKey(Item)
     rtype = models.CharField(max_length=3, choices=CHOICES)
-    value = models.CharField(max_length=20)
+    cvalue = models.CharField(max_length=20, blank=True)
+    ivalue = models.IntegerField(blank=True)
+
+    def __str__(self):
+        if self.rtype == Restriction.MIN_DUR or self.rtype == Restriction.MAX_DUR:
+            return '%s of %dmin for %s' % (self.rtype, self.ivalue, self.item)
+        else:
+            return '%s (%s)' % (self.rtype, self.item)
